952ae52d180833969de144212ede28163998276f
Fatemeh Danafar <153620606+FatemehDanafar@users.noreply.github.com>
2023-12-24 00:35:05 +0330

    Add files via upload

---
+++ Practice2.py
@@ -0,0 +1,4 @@
+Radius=int(input("Enter Radius: "))
+P=3.14
+print("Mohit: " ,Radius*2*P)
+print("Masahat: " ,Radius**2*P)
---
+++ Practice3.py
@@ -0,0 +1,7 @@
+a=float(input("Enter Number 1: "))
+b=float(input("Enter Number 2: "))
+sum=a+b
+subtract=a-b
+multiplication=a*b
+division=a/b
+ print("sum:" ,sum, "\nsubtract:" ,subtract, "\nmultiplicatio:" ,multiplication, "\ndivisio:" ,division) 
